e2f84f4e67f8cd85f11d4bfb7c22a29b73bdce62
balaraju143 <yarapatinibalaraju143@gmail.com>
2026-06-01 11:47:21 +0530

    first commit

--- script.js
+++ script.js
@@ -295,3 +295,27 @@ aboutElements.forEach((element)=>{
     aboutObserver.observe(element);
 
 });
+
+
+const fadeElements =
+document.querySelectorAll(".fade-up");
+
+const observer =
+new IntersectionObserver((entries)=>{
+
+    entries.forEach((entry)=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+fadeElements.forEach((el)=>{
+    observer.observe(el);
+});
